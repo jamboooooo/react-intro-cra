a20bebdbb0240e80afcaca59545f830260d850eb
jamboooooo <hisllam35@gmail.com>
2021-07-05 22:17:44 +0300

    commit

---
+++ first-cra-app/src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+ReactDOM.render(
+    <div>
+        <h1>Hello, world</h1>
+        <p>Lorem ipsum</p>
+    </div>
+, document.getElementById('root'))
